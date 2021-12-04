c170422886d38f2a7c24fbe527ec2f906940c44a
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2021-12-04 07:18:48 -0800

    pause()

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const video = document.querySelector(".video-container");
 btn.addEventListener("click", function () {
     if (!btn.classList.contains("slide")) {
         btn.classList.add("slide");
+        video.pause();
     }
     else {
         btn.classList.remove("slide");
