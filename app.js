b0b50787ed9c1dbc8c0cd94ca0d59ca846c49c6c
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2021-12-04 07:15:58 -0800

    classList.contains()

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const btn = document.querySelector(".switch-btn");
 const video = document.querySelector(".video-container");
 
-btn.addEventListener("click", function ()
+btn.addEventListener("click", function () {
+    if (!btn.classList.contains("slide"))
+}
